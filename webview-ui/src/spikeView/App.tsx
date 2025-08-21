823572568da4006048eafb20912b6f3ed020338b
Geronimo <gpalavecino@fi.uba.ar>
2025-08-21 15:38:12 -0300

    feature: statically generated rpc bindings + view switching buttons

--- webview-ui/src/spikeView/App.tsx
+++ webview-ui/src/spikeView/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { vscode } from '../vscode'
 
 export default function App() {
   const [content, setContent] = useState<string>('')
 
-  window.addEventListener('message', (event: MessageEvent) => {
-    const message = event.data
-    setContent(message.content)
-  })
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      const message = event.data;
+      setContent(message.contents)
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    vscode.postMessage({type: 'ready'});
+
+    return () => {
+      window.removeEventListener('message', handleMessage)
+    }
+  }, [])
 
   function onEditClick() {
     vscode.postMessage({ type: 'edit', data: 'Good Bye' })
