43187baecb94bf62500dbae42fa7dd18db1ac51b
Patricio Tourne Passarino <contact@patricios.space>
2025-10-02 20:19:30 -0300

    fix: revert hardcoded tree

--- webview-ui/src/structuredView/App.tsx
+++ webview-ui/src/structuredView/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { NodeRender } from './components/line'
 import { LineProvider } from './components/lineContext'
-// import { vscode } from '../vscode'
+import { vscode } from '../vscode'
 import * as nodes from '../../../src/nodes/cNodes';
 
 export default function App() {
@@ -10,48 +10,19 @@ export default function App() {
   const [selectedKey, setSelectedKey] = useState<string | null>(null);
   const [insertTargetId, setInsertTargetId] = useState<string | null>(null);
 
-
-  const main: nodes.FunctionDefinition = {
-    id: '0',
-    name: 'main',
-    type: 'FunctionDefinition',
-    return_type: 'int',
-    params: [{ type: 'FunctionParameter', id: '1', name: 'argv', data_type: 'int' }],
-    body: {
-      id: '2',
-      type: 'CompoundStatement',
-      statements: [
-        {
-          id: '3',
-          type: 'Declaration',
-          name: 'myVar',
-          data_type: 'int',
-          initializer: {
-            id: '4',
-            type: "NumberLiteral",
-            value: "58"
-          } as nodes.CExpressionNode
-        } as nodes.Node
-      ]
-    }
-  };
-
-  const testAst: nodes.Node[] = [main]
-
   useEffect(() => {
-    setAst(testAst)
-    // const handleMessage = (event: MessageEvent) => {
-    //   const message = event.data;
-    //   setAst(message.contents)
-    // }
+    const handleMessage = (event: MessageEvent) => {
+      const message = event.data;
+      setAst(message.contents)
+    }
 
-    // window.addEventListener('message', handleMessage)
+    window.addEventListener('message', handleMessage)
 
-    // vscode.postMessage({type: 'ready'});
+    vscode.postMessage({ type: 'ready' });
 
-    // return () => {
-    //   window.removeEventListener('message', handleMessage)
-    // }    
+    return () => {
+      window.removeEventListener('message', handleMessage)
+    }
   }, [])
 
   const onEdit = <T extends nodes.Node, K extends string & keyof T>(node: T, key: K | null) => {
