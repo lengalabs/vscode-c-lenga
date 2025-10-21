eeaa6a4fcb15909f14077fac3b1592fc74be8683
Patricio Tourne Passarino <contact@patricios.space>
2025-10-20 23:24:45 -0300

    feat: formating and linting configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,43 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+  },
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      "@typescript-eslint": typescriptEslint,
+      prettier: prettierPlugin,
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
 
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+      ...prettierConfig.rules,
+      "prettier/prettier": "error",
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "import",
+          format: ["camelCase", "PascalCase"],
+        },
+      ],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+      curly: "warn",
+      eqeqeq: "warn",
+      "no-throw-literal": "warn",
     },
-}];
+  },
+];
